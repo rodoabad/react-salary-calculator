87fa28b59cae005c6cfeb3940fa249e2296c4555
rodoabad <rodoabad@gmail.com>
2016-07-30 11:10:15 -0500

    chore: update package configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,3 @@
 module.exports = {
-    extends: 'rodoabad',
-    rules: {
-        'no-invalid-this': 0,
-        'no-magic-numbers': 2,
-        'react/jsx-no-bind': 0,
-    }
+    extends: 'rodoabad'
 };
