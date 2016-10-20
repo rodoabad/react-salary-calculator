776c2773fe82e633131a15ff9c39fbcb16a5b559
rodoabad <rodoabad@gmail.com>
2016-10-19 21:06:54 -0500

    feat: add action creators for take home

---
+++ src/take-home/action-creators.js
@@ -0,0 +1,11 @@
+import actions from '../actions';
+
+export const updateSalary = newSalary => ({
+    newSalary,
+    type: actions.UPDATE_SALARY
+});
+
+export const updateYear = newYear => ({
+    newYear,
+    type: actions.UPDATE_YEAR
+});
---
+++ test/unit/take-home/action-creators.spec.js
@@ -0,0 +1,59 @@
+import * as actionCreators from '../../../src/take-home/action-creators';
+import actions from '../../../src/actions';
+import {expect} from 'code';
+
+describe('Given the action creators for take home', () => {
+
+    describe('when updating the salary', () => {
+
+        let updateSalaryAction;
+
+        beforeEach(() => {
+
+            updateSalaryAction = actionCreators.updateSalary(100);
+
+        });
+
+        it('should have a type', () => {
+
+            expect(updateSalaryAction.type).string().equal(actions.UPDATE_SALARY);
+
+        });
+
+        it('should have a new salary', () => {
+
+            const expectedNewSalary = 100;
+
+            expect(updateSalaryAction.newSalary).number().equal(expectedNewSalary);
+
+        });
+
+    });
+
+    describe('when updating the year', () => {
+
+        let updateYearAction;
+
+        beforeEach(() => {
+
+            updateYearAction = actionCreators.updateYear(2016);
+
+        });
+
+        it('should have a type', () => {
+
+            expect(updateYearAction.type).string().equal(actions.UPDATE_YEAR);
+
+        });
+
+        it('should have a new salary', () => {
+
+            const expectedNewYear = 2016;
+
+            expect(updateYearAction.newYear).number().equal(expectedNewYear);
+
+        });
+
+    });
+
+});
