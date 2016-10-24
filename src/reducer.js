a985c69bb359c123f7d1b78e3bf4b8e93785ff68
rodoabad <rodoabad@gmail.com>
2016-10-23 20:01:28 -0500

    refactor: reducer for salary calculator

--- src/reducer.js
+++ src/reducer.js
@@ -1,18 +1,18 @@
-import TakeHome, {getDefaultState} from './state';
+import SalaryCalculator, {getDefaultState} from './state';
 import actions from './actions';
 
 const updateSalary = (state, action) =>
-    TakeHome.update(state, {
+    SalaryCalculator.update(state, {
         salary: {
             $set: action.newSalary
         }
     });
 
-export default (state = getDefaultState(), action) => {
+const actionTypes = {
+    [actions.UPDATE_SALARY]: updateSalary
+};
 
-    const actionTypes = {
-        [actions.UPDATE_SALARY]: updateSalary
-    };
+export default (state = getDefaultState(), action) => {
 
     const reducer = actionTypes[action.type];
 
